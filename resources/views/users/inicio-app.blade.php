3c8b1cdd75e3e16ba9fd2d15cac3f291a43be04a
Jesus Arraras <jesus@gmail.es>
2022-06-12 23:59:45 +0200

    deshabilitado alpine y password

--- resources/views/users/inicio-app.blade.php
+++ resources/views/users/inicio-app.blade.php
@@ -28,17 +28,17 @@
             </div>
 
             <div class="container mt-4 flex space-x-2">
-
-                <div class="relative" x-data="{ show: false }">
-                    <x-jet-label for="password" value="{{ __('Password:') }}" />
-                    <x-input-password />
+                <div class="mt-4">
+                    <x-jet-label for="password" value="{{ __('Password') }}" />
+                    <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                        autocomplete="new-password" />
                 </div>
 
-                <div class="relative pl-3" x-data="{ show: false }">
-                    <x-jet-label for="password_confirmation" value="{{ __('Confirm Password:') }}" />
-                    <x-input-password2 />
+                <div class="mt-4 relative pl-3">
+                    <x-jet-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                    <x-jet-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                        name="password_confirmation" required autocomplete="new-password" />
                 </div>
-
             </div>
 
             @if (Laravel\Jetstream\Jetstream::hasTermsAndPrivacyPolicyFeature())
